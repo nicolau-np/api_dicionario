7409c543612321ffefde5e2e818baa919f7230d0
nicolau-np <mr1Normaliii@gmail.com>
2023-04-17 16:40:46 +0100

    update

--- database/seeders/TipoTextoSeeder.php
+++ database/seeders/TipoTextoSeeder.php
@@ -10,23 +10,23 @@ class TipoTextoSeeder extends Seeder
     static $tipo_textos =[
         [
             'tipo'=>"Alfabeto",
-            'img'=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRh8VZUWtubvm1QZ2hYoeQQqYyMoWQguI9mlA&usqp=CAU",
+            'img'=>"https://scontent.flad4-1.fna.fbcdn.net/v/t39.30808-6/341703368_605774021430603_4700868752013258666_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeG4UE_tWvss5Ma--A3iGHoi2rekYFBXW5rat6RgUFdbmtWytdGm7OVAC6TWfFyzuQSGFvqQi-yG9A8AU6MA8IDM&_nc_ohc=RImGhH0FtUMAX-dQ5Kc&_nc_zt=23&_nc_ht=scontent.flad4-1.fna&oh=00_AfBGwMxM-WaB0x9TteMnzxgSg9FiYxbJFrpNuOcGwbiYgw&oe=64434D3E",
             'estado'=>"on",
         ], [
             'tipo'=>"Questões",
-            'img'=>"https://www.freeiconspng.com/thumbs/question-mark-icon/3d-question-mark-icon-blue-color-picture-6.png",
+            'img'=>"https://scontent.flad4-1.fna.fbcdn.net/v/t39.30808-6/337290989_947887873018361_2605389124048266032_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGP5D7uDkto51Ay_s65LT3eNNfy7zJtHAk01_LvMm0cCYOnTaAcIiQRnXGniXt-f6EhIu-_Nur9ZpFKExcQfhnq&_nc_ohc=WMFiyA6CQAMAX-oZv8k&_nc_zt=23&_nc_ht=scontent.flad4-1.fna&oh=00_AfDHtyMcSL9OVnvqoCu2Pi2QILUMkB_ZU0oe1_GjEF4myw&oe=64430F34",
             'estado'=>"on",
         ],[
             'tipo'=>"Comidas",
-            'img'=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxmEnetPrdPcsaCYmooRb2kATdocOR_o7uTQ&usqp=CAU",
+            'img'=>"https://scontent.flad4-1.fna.fbcdn.net/v/t39.30808-6/336904846_616005540067510_7685025389456787576_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeEBkdyQ8vsFh1X9ZOdl4YL5qnrX3JgO0qCqetfcmA7SoKHJpv2NIncSnP1OPbyTa13Fdli_KF1xQl8MvXaYfOOE&_nc_ohc=ebczkYIJAYgAX9err_2&_nc_zt=23&_nc_ht=scontent.flad4-1.fna&oh=00_AfCaQdQg8Yw_tclQpePtrp8ukgA5Amf-Kbz1M76AgqB5Ww&oe=64433CB8",
             'estado'=>"on",
         ],[
             'tipo'=>"Saudações",
-            'img'=>"https://cdn-icons-png.flaticon.com/512/774/774502.png",
+            'img'=>"https://scontent.flad4-1.fna.fbcdn.net/v/t39.30808-6/336909476_225904840029121_68048960231751415_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeHPTG0IU0ta9Fp2GLfT7InWrRVZjKXVxWatFVmMpdXFZh28wOu8zNoEE8KIohtx3oZHlos22d4ww2LUY_ho4p1p&_nc_ohc=-ksJJMZ1YggAX9UUglL&_nc_zt=23&_nc_ht=scontent.flad4-1.fna&oh=00_AfACZ-CSP-KeMerA_9pJJOVy8tHyfE91ApIabNettR-SQw&oe=6441796C",
             'estado'=>"on",
         ],[
             'tipo'=>"Números",
-            'img'=>"https://img.freepik.com/free-vector/counting-number-0-9-math-symbols_1308-102627.jpg?w=2000",
+            'img'=>"https://scontent.flad4-1.fna.fbcdn.net/v/t39.30808-6/336926826_1362436487866818_9000559513188232575_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGh8uRsC_pAHTbjb9tRL-Lj7SwgItH6MYHtLCAi0foxgZNBpm5t0ujBXDw8DNM3EzXyRISxiDlShfqifc5G34Xc&_nc_ohc=oReW-_A26BcAX9NT_Px&_nc_zt=23&_nc_ht=scontent.flad4-1.fna&oh=00_AfDh-KvkAVaNcWnIEQ1Xv8e8G_gyh8s1ZJ46n2VgnIKcJg&oe=6441D38E",
             'estado'=>"on",
         ],
     ];
